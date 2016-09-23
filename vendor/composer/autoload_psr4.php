d596aff8726b5cf4303fed430758383dd3f2c779
Freitas <sun.freitas@gmail.com>
2016-09-23 11:44:26 -0300

    Add the Symfony Event Dispatcher Component

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
     'Api\\' => array($baseDir . '/api'),
 );
