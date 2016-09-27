1c131ef550d758eff4405ad406dc63233d46e8c6
Freitas <sun.freitas@gmail.com>
2016-09-26 23:04:58 -0300

    Add basic routes

--- public/index.php
+++ public/index.php
@@ -1,18 +1,20 @@
 <?php
 include('../vendor/autoload.php');
-$collection = include('../routes/api.php');
+
 // Defining routes;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
-// use Symfony\Component\Config\FileLocator;
-// use Symfony\Component\Routing\Loader\PhpFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\PhpFileLoader;
 
 use Symfony\Component\HttpFoundation\Request;
 
 // Agora eu carrego os arquivos de rotas aqui.
-// $locator = new FileLocator(array(__DIR__.'/routes'));
-// $loader = new PHPFileLoader($locator);
-// $collection = $loader->load('api.php');
+$base_api = dirname(__DIR__);
+
+$locator = new FileLocator(array("{$base_api}/routes"));
+$loader = new PhpFileLoader($locator);
+$collection = $loader->load('api.php');
 
 /**
  * @todo O que vem primeiro? A galinha ou ou ovo? Acho que as rotas devem variant_date_from_timestamp
@@ -28,8 +30,14 @@ $context->fromRequest($request);
 $matcher = new UrlMatcher($collection, $context);
 
 try {
-  $parameters = $matcher->match('/home');
-  var_dump($parameters);
+  $parameters = $matcher->match($request->server->get('PATH_INFO'));
 } catch (ResourceNotFoundException $e) {
   echo $e->getMessage();
+}
+
+// Vamos verificar se existe o controlador
+if (class_exists($parameters['controller'])) {
+  $classe = $parameters['controller'];
+  $controller = new $classe;
+  call_user_func_array(array($controller, $parameters['method']), []);
 }
--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,12 @@ use Symfony\Component\Routing\Route;
  * @todo Esta parte tem que ser reduzida a uma função.
  * @todo E ficar isolada em um outro arquivo de preferência. Sugestão: RouteProvider.php
  */
-$route = new Route('/home', array('controller' => 'Home', 'method' => 'index'));
 $routes = new RouteCollection();
-$routes->add('home', $route);
+
+$home = new Route('/home', array('controller' => 'Api\Home', 'method' => 'index'));
+$routes->add('home', $home);
+
+$create = new Route('/home/create', array('controller' => 'Api\Home', 'method' => 'create'));
+$routes->add('create', $create);
 
 return $routes;
